45324964c869e3cad7610014032d890a1d7e764f
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-06-21 23:16:08 -0300

    Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import './styles/global.scss';
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
